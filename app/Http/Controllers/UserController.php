b670b4a099375700caf1da67859d75786293be4a
Nervalina Adzra Nora Aqilla <nervalinaadzranoraaqilla@gmail.com>
2025-04-25 13:44:59 +0700

    JS 4 - Prak 2.3

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,24 +2,16 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\UserModel;
 use Illuminate\Http\Request;
+use App\Models\UserModel;
+use Illuminate\Support\Facades\Hash;
+
 
 class UserController extends Controller
 {
     public function index()
     {
-        // Data yang akan diupdate
-        $data = [
-            'nama' => 'Pelanggan Pertama',  // Update kolom 'nama'
-        ];
-
-        // Update data user dengan 'username' = 'customer-1'
-        UserModel::where('username', 'customer-1')->update($data);
-
-        // Akses semua data dari tabel m_user
-        $user = UserModel::all();  // Ambil semua data dari tabel m_user
-
-        return view('user', ['data' => $user]);  // Kirim data ke view
+        $user = UserModel::where('level_id', 2)->count();
+        return view('user', ['data' => $user]);  
     }
 }
