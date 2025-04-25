dcf45552e94006b4e687addb2d742bea7d3da139
Nervalina Adzra Nora Aqilla <nervalinaadzranoraaqilla@gmail.com>
2025-04-25 15:15:35 +0700

    JS 4 - Prak 2.6

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -5,13 +5,26 @@
 </head>
 <body>
     <h1>Data User</h1>
+    <a href="/user/tambah">+ Tambah User</a>
     <table border="1" cellpadding="2" cellspacing="0">
-        <tr>
-            <th>Jumlah Pengguna</th>
+        <tr> <!-- ini tadi typo: tr tanpa < -->
+            <th>ID</th>
+            <th>Username</th>
+            <th>Nama</th>
+            <th>ID Level Pengguna</th>
+            <th>Aksi</th>
         </tr>
-        <tr>
-            <td>{{ $data }}</td>
+        @foreach ($data as $d)
+        <tr> <!-- ini tadi typo: ktr -->
+            <td>{{ $d->user_id }}</td>
+            <td>{{ $d->username }}</td>
+            <td>{{ $d->nama }}</td>
+            <td>{{ $d->level_id }}</td>
+            <td>
+                <a href="/user/ubah/{{ $d->user_id }}">Ubah</a> |
+                <a href="/user/hapus/{{ $d->user_id }}">Hapus</a>
+            </td>
         </tr>
+        @endforeach
     </table>
 </body>
-</html>
